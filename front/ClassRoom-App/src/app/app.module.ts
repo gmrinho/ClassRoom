95060fe965cc48ad1b68675c3b06d26abda8e27d
Samuel <samueldasilva129@gmail.com>
2022-10-17 13:49:14 -0300

    Front 1.1

--- front/ClassRoom-App/src/app/app.module.ts
+++ front/ClassRoom-App/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlocosComponent } from './blocos/blocos.component';
 import { AulasComponent } from './aulas/aulas.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
       BlocosComponent,
-      AulasComponent
+      AulasComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
